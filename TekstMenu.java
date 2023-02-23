262ccae811568c93c0a40029b27cad50077e5280
Kevin D. Mortensen <kevi5221@edu.zealand.dk>
2023-02-23 13:41:41 +0100

    Færdig bestil en tid

--- TekstMenu.java
+++ TekstMenu.java
@@ -29,7 +29,6 @@ public class TekstMenu {
         Tidbestilling fredag3 = new Tidbestilling();
         Tidbestilling fredag4 = new Tidbestilling();
 
-
         for (int i = 0; i < mandagListe.size(); i++) {
             switch (mandagListe.get(i).getStartModul()) {
                 case 1:
@@ -46,7 +45,6 @@ public class TekstMenu {
                     break;
             }
         }
-
 
         for (int i = 0; i < tirsdagListe.size(); i++) {
             switch (tirsdagListe.get(i).getStartModul()) {
@@ -117,7 +115,7 @@ public class TekstMenu {
         }
 
 
-
+        //Printer Skemaet
         System.out.printf(" %8s %8s %8s %8s %8s","        MANDAG","  TIRSDAG","  ONSDAG ","  TORSDAG","  FREDAG");
         System.out.println();
         System.out.printf("%-7s", " 8 - 9");
@@ -133,21 +131,281 @@ public class TekstMenu {
         System.out.printf("  %-8.8s  %-8.8s  %-8.8s  %-8.8s  %-8.8s", mandag4.getKundenavn(), tirsdag4.getKundenavn(),onsdag4.getKundenavn(),torsdag4.getKundenavn(),fredag4.getKundenavn());
     }
 
-    public static void main(String[] args) {
-        LocalDate mandag = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
-        LocalDate tirsdag = mandag.plusDays(1);
-        LocalDate onsdag = mandag.plusDays(2);
-        LocalDate torsdag = mandag.plusDays(3);
-        LocalDate fredag = mandag.plusDays(4);
-
+    public static void BestilEnTid(ArrayList<Tidbestilling> mandagListe, ArrayList<Tidbestilling> tirsdagListe, ArrayList<Tidbestilling> onsdagListe, ArrayList<Tidbestilling> torsdagListe, ArrayList<Tidbestilling>fredagListe,ArrayList<Integer>mandagModul,ArrayList<Integer>tirsdagModul, ArrayList<Integer>onsdagModul, ArrayList<Integer>torsdagModul,ArrayList<Integer>fredagModul)
+    {
         UseCaseController ucc = new UseCaseController();
-        ucc.opretTidbestilling(1,1, mandag, 1,"emil", "88888888");
-        ucc.opretTidbestilling(1,1, tirsdag, 4,"sercan", "55888888");
-        ucc.opretTidbestilling(1,1, onsdag, 4,"emil", "88888888");
-        ucc.opretTidbestilling(1,1, torsdag, 4,"peterjensen", "88888888");
-        menu();
-    }
+        Tidbestilling mandag1 = new Tidbestilling();
+        Tidbestilling mandag2 = new Tidbestilling();
+        Tidbestilling mandag3 = new Tidbestilling();
+        Tidbestilling mandag4 = new Tidbestilling();
+        Tidbestilling tirsdag1 = new Tidbestilling();
+        Tidbestilling tirsdag2 = new Tidbestilling();
+        Tidbestilling tirsdag3 = new Tidbestilling();
+        Tidbestilling tirsdag4 = new Tidbestilling();
+        Tidbestilling onsdag1 = new Tidbestilling();
+        Tidbestilling onsdag2 = new Tidbestilling();
+        Tidbestilling onsdag3 = new Tidbestilling();
+        Tidbestilling onsdag4 = new Tidbestilling();
+        Tidbestilling torsdag1 = new Tidbestilling();
+        Tidbestilling torsdag2 = new Tidbestilling();
+        Tidbestilling torsdag3 = new Tidbestilling();
+        Tidbestilling torsdag4 = new Tidbestilling();
+        Tidbestilling fredag1 = new Tidbestilling();
+        Tidbestilling fredag2 = new Tidbestilling();
+        Tidbestilling fredag3 = new Tidbestilling();
+        Tidbestilling fredag4 = new Tidbestilling();
 
+        String [] mandagLedig = {"Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig"};
+        String [] tirsdagLedig = {"Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig"};
+        String [] onsdagLedig = {"Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig"};
+        String [] torsdagLedig = {"Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig"};
+        String [] fredagLedig = {"Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig","Ledig"};
+
+        for(int i=0; i<mandagListe.size();i++) {
+            switch (mandagListe.get(i).getStartModul()) {
+                case 1:
+                    mandagLedig[0] = "X";
+                    if (mandagModul.get(i) == 2) {
+                        mandagLedig[1] = "X";
+                    }
+                    if (mandagModul.get(i) == 3) {
+                        mandagLedig[1] = "X";
+                        mandagLedig[2] = "X";
+                    }
+                    break;
+                case 2:
+                    mandagLedig[1] = "X";
+                    if (mandagModul.get(i) == 2) {
+                        mandagLedig[2] = "X";
+                    }
+                    if (mandagModul.get(i) == 3) {
+                        mandagLedig[2] = "X";
+                        mandagLedig[3] = "X";
+                    }
+                    break;
+                case 3:
+                    mandagLedig[2] = "X";
+                    if (mandagModul.get(i) == 2) {
+                        mandagLedig[3] = "X";
+                    }
+                    if (mandagModul.get(i) == 3) {
+                        mandagLedig[3] = "X";
+                        mandagLedig[4] = "X";
+                    }
+                    break;
+                case 4:
+                    mandagLedig[3] = "X";
+                    if (mandagModul.get(i) == 2) {
+                        mandagLedig[4] = "X";
+                    }
+                    if (mandagModul.get(i) == 3) {
+                        mandagLedig[4] = "X";
+                        mandagLedig[5] = "X";
+                        break;
+
+                    }
+            }
+        }
+        for(int i=0; i<tirsdagListe.size();i++) {
+            switch (tirsdagListe.get(i).getStartModul()) {
+                case 1:
+                    tirsdagLedig[0]="X";
+                    if (tirsdagModul.get(i) == 2) {
+                        tirsdagLedig[1]="X";
+                    }
+                    if (tirsdagModul.get(i) == 3) {
+                        tirsdagLedig[1]="X";
+                        tirsdagLedig[2]="X";
+                    }
+                    break;
+                case 2:
+                    tirsdagLedig[1]="X";
+                    if (tirsdagModul.get(i) == 2) {
+                        tirsdagLedig[2]="X";
+                    }
+                    if (tirsdagModul.get(i) == 3) {
+                        tirsdagLedig[2]="X";
+                        tirsdagLedig[3]="X";
+                    }
+                    break;
+                case 3:
+                    tirsdagLedig[2]="X";
+                    if (tirsdagModul.get(i) == 2) {
+                        tirsdagLedig[3]="X";
+                    }
+                    if (tirsdagModul.get(i) == 3) {
+                        tirsdagLedig[3]="X";
+                        tirsdagLedig[4]="X";
+                    }
+                    break;
+                case 4:
+                    tirsdagLedig[3]="X";
+                    if (tirsdagModul.get(i) == 2) {
+                        tirsdagLedig[4]="X";
+                    }
+                    if (tirsdagModul.get(i) == 3) {
+                        tirsdagLedig[4]="X";
+                        tirsdagLedig[5]="X";
+                    }
+                    break;
+            }
+        }
+        for(int i=0; i<onsdagListe.size();i++)
+        {
+            switch(onsdagListe.get(i).getStartModul())
+            {
+                case 1:
+                    onsdagLedig[0]="X";
+                    if (onsdagModul.get(i) == 2) {
+                        onsdagLedig[1]="X";
+                    }
+                    if (onsdagModul.get(i) == 3) {
+                        onsdagLedig[1]="X";
+                        onsdagLedig[2]="X";
+                    }
+                    break;
+                case 2:
+                    onsdagLedig[1]="X";
+                    if (onsdagModul.get(i) == 2) {
+                        onsdagLedig[2]="X";
+                    }
+                    if (onsdagModul.get(i) == 3) {
+                        onsdagLedig[2]="X";
+                        onsdagLedig[3]="X";
+                    }
+                    break;
+                case 3:
+                    onsdagLedig[2]="X";
+                    if (onsdagModul.get(i) == 2) {
+                        onsdagLedig[3]="X";
+                    }
+                    if (onsdagModul.get(i) == 3) {
+                        onsdagLedig[3]="X";
+                        onsdagLedig[4]="X";
+                    }
+                    break;
+                case 4:
+                    onsdagLedig[3]="X";
+                    if (onsdagModul.get(i) == 2) {
+                        onsdagLedig[4]="X";
+                    }
+                    if (onsdagModul.get(i) == 3) {
+                        onsdagLedig[4]="X";
+                        onsdagLedig[5]="X";
+                    }
+                    break;
+            }
+        }
+        for(int i=0; i<torsdagListe.size();i++)
+        {
+            switch(torsdagListe.get(i).getStartModul())
+            {
+                case 1:
+                    torsdagLedig[0]="X";
+                    if (torsdagModul.get(i) == 2) {
+                        torsdagLedig[1]="X";
+                    }
+                    if (torsdagModul.get(i) == 3) {
+                        torsdagLedig[1]="X";
+                        torsdagLedig[2]="X";
+                    }
+                    break;
+                case 2:
+                    torsdagLedig[1]="X";
+                    if (torsdagModul.get(i) == 2) {
+                        torsdagLedig[2]="X";
+                    }
+                    if (torsdagModul.get(i) == 3) {
+                        torsdagLedig[2]="X";
+                        torsdagLedig[3]="X";
+                    }
+                    break;
+                case 3:
+                    torsdagLedig[2]="X";
+                    if (torsdagModul.get(i) == 2) {
+                        torsdagLedig[3]="X";
+                    }
+                    if (torsdagModul.get(i) == 3) {
+                        torsdagLedig[3]="X";
+                        torsdagLedig[4]="X";
+                    }
+                    break;
+                case 4:
+                    torsdagLedig[3]="X";
+                    if (torsdagModul.get(i) == 2) {
+                        torsdagLedig[4]="X";
+                    }
+                    if (torsdagModul.get(i) == 3) {
+                        torsdagLedig[4]="X";
+                        torsdagLedig[5]="X";
+                    }
+                    break;
+            }
+
+        }
+        for(int i=0; i<fredagListe.size();i++)
+        {
+            switch(fredagListe.get(i).getStartModul())
+            {
+                case 1:
+                    fredagLedig[0]="X";
+                    if (fredagModul.get(i) == 2) {
+                        fredagLedig[1]="X";
+                    }
+                    if (fredagModul.get(i) == 3) {
+                        fredagLedig[1]="X";
+                        fredagLedig[2]="X";
+                    }
+                    break;
+                case 2:
+                    fredagLedig[1]="X";
+                    if (fredagModul.get(i) == 2) {
+                        fredagLedig[2]="X";
+                    }
+                    if (fredagModul.get(i) == 3) {
+                        fredagLedig[2]="X";
+                        fredagLedig[3]="X";
+                    }
+                    break;
+                case 3:
+                    fredagLedig[2]="X";
+                    if (fredagModul.get(i) == 2) {
+                        fredagLedig[3]="X";
+                    }
+                    if (fredagModul.get(i) == 3) {
+                        fredagLedig[3]="X";
+                        fredagLedig[4]="X";
+                    }
+                    break;
+                case 4:
+                    fredagLedig[3]="X";
+                    if (fredagModul.get(i) == 2) {
+                        fredagLedig[4]="X";
+                    }
+                    if (fredagModul.get(i) == 3) {
+                        fredagLedig[4]="X";
+                        fredagLedig[5]="X";
+                    }
+                    break;
+            }
+
+        }
+        //Printer Skemaet
+        System.out.printf(" %8s %8s %8s %8s %8s","        MANDAG","  TIRSDAG","  ONSDAG ","  TORSDAG","  FREDAG");
+        System.out.println();
+        System.out.printf("%-7s", " 8 - 9");
+        System.out.printf("  %-8.8s  %-8.8s  %-8.8s  %-8.8s  %-8.8s", mandagLedig[0], tirsdagLedig[0],onsdagLedig[0],torsdagLedig[0],fredagLedig[0]);
+        System.out.println();
+        System.out.printf("%-7s", " 9 - 10");
+        System.out.printf("  %-8.8s  %-8.8s  %-8.8s  %-8.8s  %-8.8s", mandagLedig[1], tirsdagLedig[1],onsdagLedig[1],torsdagLedig[1],fredagLedig[1]);
+        System.out.println();
+        System.out.printf("%-7s", "10 - 11");
+        System.out.printf("  %-8.8s  %-8.8s  %-8.8s  %-8.8s  %-8.8s", mandagLedig[2], tirsdagLedig[2],onsdagLedig[2],torsdagLedig[2],fredagLedig[2]);
+        System.out.println();
+        System.out.printf("%-7s", "11 - 12");
+        System.out.printf("  %-8.8s  %-8.8s  %-8.8s  %-8.8s  %-8.8s", mandagLedig[3], tirsdagLedig[3],onsdagLedig[3],torsdagLedig[3],fredagLedig[3]);
+
+    }
 
     public static void menu() {
         Scanner input = new Scanner(System.in);
@@ -182,15 +440,80 @@ public class TekstMenu {
                 printSkema(mandagListe, tirsdagListe, onsdagListe, torsdagListe, fredagListe);
                 break;
             case 2:
-                System.out.println("Indtast tidbestilling oplysninger: ");
-                ucc.opretTidbestilling(input.nextInt(), input.nextInt(), LocalDate.now(), input.nextInt(), input.next(), input.next());
+                System.out.println("Vælg en behandling: ");
+                int behandlingID= input.nextInt();
+                System.out.println("Vælg en Frisør");
+                int medarbejderID= input.nextInt();
+                LocalDate mandagtid = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+                LocalDate tirsdagtid = mandagtid.plusDays(1);
+                LocalDate onsdagtid = mandagtid.plusDays(2);
+                LocalDate torsdagtid = mandagtid.plusDays(3);
+                LocalDate fredagtid = mandagtid.plusDays(4);
 
+                ArrayList<Tidbestilling> mandagListetid = ucc.seLedigeTider(medarbejderID, mandagtid);
+                ArrayList<Tidbestilling> tirsdagListetid = ucc.seLedigeTider(medarbejderID, tirsdagtid);
+                ArrayList<Tidbestilling> onsdagListetid = ucc.seLedigeTider(medarbejderID, onsdagtid);
+                ArrayList<Tidbestilling> torsdagListetid = ucc.seLedigeTider(medarbejderID, torsdagtid);
+                ArrayList<Tidbestilling> fredagListetid = ucc.seLedigeTider(medarbejderID, fredagtid);
+
+                ArrayList<Integer> mandagModul = ucc.hentAntalModuler(mandagListetid);
+                ArrayList<Integer> tirsdagModul = ucc.hentAntalModuler(tirsdagListetid);
+                ArrayList<Integer> onsdagModul = ucc.hentAntalModuler(onsdagListetid);
+                ArrayList<Integer> torsdagModul = ucc.hentAntalModuler(torsdagListetid);
+                ArrayList<Integer> fredagModul = ucc.hentAntalModuler(fredagListetid);
+
+                BestilEnTid(mandagListetid,tirsdagListetid,onsdagListetid,torsdagListetid,fredagListetid,mandagModul,tirsdagModul,onsdagModul,torsdagModul,fredagModul);
+                System.out.println("");
+                System.out.println("Vælg en dag: 1-5");
+                int dag= input.nextInt();
+                System.out.println("Vælg et modul: 1-4");
+                int modul= input.nextInt();
+                LocalDate dato = LocalDate.now();
+                System.out.println("Navn:");
+                String navn = input.next();
+                System.out.println("Telefonnummer:");
+                String tlf = input.next();
+                switch (dag)
+                {
+                    case 1:
+                        dato = mandagtid;
+                        break;
+                    case 2:
+                        dato = tirsdagtid;
+                        break;
+                    case 3:
+                        dato = onsdagtid;
+                        break;
+                    case 4:
+                        dato =torsdagtid;
+                        break;
+                    case 5:
+                        dato =fredagtid;
+                        break;
+                }
+                ucc.opretTidbestilling(behandlingID,medarbejderID,dato,modul,navn,tlf);
                 break;
+
             case 3:
+                //Her skal skema vises
+
+                System.out.println("Indtast nummeret på den bestilling du vil slette");
+                int IDslet = input.nextInt();
+                ucc.sletTidbestilling(IDslet);
 
                 break;
             case 4:
+                System.out.println("Indtast en medarbejder og en dato: ");
+                int inuptMedID = input.nextInt();
 
+                LocalDate m = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+                LocalDate ti = m.plusDays(1);
+                LocalDate o = m.plusDays(2);
+                LocalDate to = m.plusDays(3);
+                LocalDate f = m.plusDays(4);
+
+                ArrayList<Tidbestilling> tbs = ucc.seLedigeTider(inuptMedID,m);
+                System.out.println(tbs);
                 break;
             case 5:
 
@@ -199,8 +522,25 @@ public class TekstMenu {
 
 
         }
+    }
+    public static void main(String[] args) {
+        LocalDate mandag = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+        LocalDate tirsdag = mandag.plusDays(1);
+        LocalDate onsdag = mandag.plusDays(2);
+        LocalDate torsdag = mandag.plusDays(3);
+        LocalDate fredag = mandag.plusDays(4);
+
+        //UseCaseController ucc = new UseCaseController();
+       // ucc.opretTidbestilling(2,1, fredag, 2,"emil", "88888888");
+        //ucc.opretTidbestilling(2,1, tirsdag, 1,"sercan", "55888888");
+
+        menu();
+    }
+
+
+
 
 
     }
-}
+
 
